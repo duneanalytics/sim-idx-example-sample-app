d0c1b3975596616376d2ecb2d43a8cced699acb9
Adrien Hamelin <adrien@dune.com>
2025-09-18 13:40:36 +0200

    Reduce scope of the sample app because it is used often

--- listeners/src/Main.sol
+++ listeners/src/Main.sol
@@ -9,15 +9,15 @@ contract Triggers is BaseTriggers {
     function triggers() external virtual override {
         UniswapV3FactoryListener listener = new UniswapV3FactoryListener();
         addTrigger(
-            chainContract(Chains.Ethereum, 0x1F98431c8aD98523631AE4a59f267346ea31F984),
+            chainContract(Chains.Ethereum.withBlockRange(22000000, 23000000), 0x1F98431c8aD98523631AE4a59f267346ea31F984),
             listener.triggerOnCreatePoolFunction()
         );
         addTrigger(
-            chainContract(Chains.Unichain, 0x1F98400000000000000000000000000000000003),
+            chainContract(Chains.Unichain.withBlockRange(21000000, 22000000), 0x1F98400000000000000000000000000000000003),
             listener.triggerOnCreatePoolFunction()
         );
         addTrigger(
-            chainContract(Chains.Base, 0x33128a8fC17869897dcE68Ed026d694621f6FDfD),
+            chainContract(Chains.Base.withBlockRange(34000000, 35000000), 0x33128a8fC17869897dcE68Ed026d694621f6FDfD),
             listener.triggerOnCreatePoolFunction()
         );
     }
